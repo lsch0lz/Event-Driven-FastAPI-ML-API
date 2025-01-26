8c82b62995eb0c67b82c8ec3dc5a1cd9e68d3ac4
Lukas Scholz <lukas.scholz@deutschebahn.com>
2025-01-26 11:20:00 +0100

    feat: basic security

--- app/api/main.py
+++ app/api/main.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import inference, results, status
+from app.api.routes import inference, results, status, auth
 
 api_router = APIRouter()
 
 api_router.include_router(inference.router)
 api_router.include_router(results.router)
 api_router.include_router(status.router)
+api_router.include_router(auth.router)
---
+++ app/api/routes/auth.py
@@ -0,0 +1,27 @@
+from typing import Annotated
+
+from fastapi import APIRouter, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.core.auth.security import fake_users_db, UserInDB, fake_hash_password, User, get_current_active_user
+
+router = APIRouter(prefix="/token", tags=["token"])
+
+@router.post("/")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@router.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
